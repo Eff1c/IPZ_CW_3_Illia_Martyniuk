12e3ee45382da08c64e5e99be8637970b00d9765
eff1c <illia.martyniuk@busmarket.ua>
2024-03-13 15:20:59 +0200

    feat: Add background

--- app/src/main/java/ua/edu/lntu/ipz_cw/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/ipz_cw/MainActivity.kt
@@ -16,17 +16,20 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import ua.edu.lntu.ipz_cw.R
 import ua.edu.lntu.ipz_cw.ui.theme.IPZ_CW_3_Illia_MartyniukTheme
 
 class MainActivity : ComponentActivity() {
@@ -69,6 +72,9 @@ fun MainScreen(modifier: Modifier = Modifier) {
                         .fillMaxWidth()
                         .padding(8.dp),
                     shape = RoundedCornerShape(8.dp),
+                    colors = CardDefaults.cardColors(
+                        containerColor = colorResource(id = R.color.purple_200),
+                    ),
                 ) {
                     Column(
                         modifier = Modifier.padding(8.dp)
